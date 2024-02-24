e081ed706a44d0d64054d65df840f0fa1bf23401
Nevena Firkova <nevena.firkova4@gmail.com>
2024-02-24 13:02:15 +0200

    MainMenu component added

---
+++ NovelNest/Components/MainMenuComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NovelNest.Components
+{
+    public class MainMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
