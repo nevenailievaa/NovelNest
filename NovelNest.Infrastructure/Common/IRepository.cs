d6de5f2801888bc4d91412ebe0e42fc710ce9ead
Nevena Firkova <nevena.firkova4@gmail.com>
2024-03-03 14:08:19 +0200

    Added Repository for the project

---
+++ NovelNest.Infrastructure/Common/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace NovelNest.Infrastructure.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+        IQueryable<T> AllAsReadOnly<T>() where T : class;
+    }
+}
---
+++ NovelNest.Infrastructure/Common/Repository.cs
@@ -0,0 +1,30 @@
+﻿namespace NovelNest.Infrastructure.Common
+{
+    using Microsoft.EntityFrameworkCore;
+    using NovelNest.Infrastructure.Data;
+
+    public class Repository : IRepository
+    {
+        private readonly DbContext dbContext;
+
+        public Repository(NovelNestDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllAsReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking();
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return dbContext.Set<T>();
+        }
+    }
+}
