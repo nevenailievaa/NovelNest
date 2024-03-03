fbd3715dd89d96299c54b25d9b99ee5830a8effc
Nevena Firkova <nevena.firkova4@gmail.com>
2024-03-03 16:31:54 +0200

    Edited the viewmodels for the index and the books pages

--- NovelNest.Core/ViewModels/Book/BookAllViewModel.cs
+++ NovelNest.Core/ViewModels/Book/BookAllViewModel.cs
@@ -7,5 +7,8 @@
         public string Author { get; set; } = null!;
         public string ImageUrl { get; set; } = null!;
         public decimal Price { get; set; }
+        public int Pages { get; set; }
+        public string PublishingHouse { get; set; } = null!;
+        public int YearPublished { get; set; }
     }
 }
