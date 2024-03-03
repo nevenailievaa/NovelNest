3991be6db7d777717a5a7b6f318b8c7adbb389e2
Nevena Firkova <nevena.firkova4@gmail.com>
2024-03-03 17:01:10 +0200

    Added claims principal extensions

---
+++ NovelNest/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace NovelNest.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
