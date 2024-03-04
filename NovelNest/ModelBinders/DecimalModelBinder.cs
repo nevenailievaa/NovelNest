f1970de8d553d113e559042d893657861cb8a9d9
Nevena Firkova <nevena.firkova4@gmail.com>
2024-03-04 11:32:51 +0200

    Decimal ModelBinder implemented

---
+++ NovelNest/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,41 @@
+﻿namespace NovelNest.ModelBinders
+{
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using System.Globalization;
+
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider
+                .GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None && !string.IsNullOrEmpty(valueResult.FirstValue))
+            {
+                decimal result = 0M;
+                bool success = false;
+
+                try
+                {
+                    string strValue = valueResult.FirstValue.Trim();
+                    strValue = strValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    strValue = strValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    result = Convert.ToDecimal(strValue, CultureInfo.CurrentCulture);
+                    success = true;
+                }
+                catch (FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+
+                if (success)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(result);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ NovelNest/ModelBinders/DecimalModelBinderProvider.cs
@@ -0,0 +1,22 @@
+﻿namespace NovelNest.ModelBinders
+{
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+    public class DecimalModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (context.Metadata.ModelType == typeof(decimal) || context.Metadata.ModelType == typeof(decimal?))
+            {
+                return new DecimalModelBinder();
+            }
+
+            return null;
+        }
+    }
+}
