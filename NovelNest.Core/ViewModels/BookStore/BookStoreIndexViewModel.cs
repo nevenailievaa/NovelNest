0b2a07d107e176515126c041cd56c4bcce871e85
Nevena Firkova <nevena.firkova4@gmail.com>
2024-03-16 14:29:52 +0200

    Separated the types of models

--- NovelNest.Core/ViewModels/BookStore/BookStoreIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NovelNest.Core.ViewModels.BookStore
-{
-    public class BookStoreIndexViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string ImageUrl { get; set; } = null!;
-    }
-}
