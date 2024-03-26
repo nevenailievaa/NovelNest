b3f3375aefcb2d5f60666c3be4c750c48aebadaf
Nevena Firkova <nevena.firkova4@gmail.com>
2024-03-26 21:13:15 +0200

    Added CSRF Security

--- NovelNest/Program.cs
+++ NovelNest/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using NovelNest.ModelBinders;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddApplicationIdentity(builder.Configuration);
 builder.Services.AddControllersWithViews(options =>
 {
     options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+    options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
 });
 
 builder.Services.AddApplicationServices();
