f8f3a33501d03cdcc0d8a37e38dc11ed4eebb0ad
Nevena Firkova <nevena.firkova4@gmail.com>
2024-04-08 20:57:21 +0300

    Added Unit Testing project

---
+++ NovelNest.UnitTests/BookUnitTests.cs
@@ -0,0 +1,17 @@
+namespace NovelNest.UnitTests
+{
+    [TestFixture]
+    public class BookUnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
